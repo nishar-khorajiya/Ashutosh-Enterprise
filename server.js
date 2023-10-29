81bd97758fcc030052e2087f8d53dda13590e748
Nishar Khorajiya <110724795+nishar-khorajiya@users.noreply.github.com>
2023-10-29 19:39:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,12 +24,14 @@ const __dirname=path.dirname(__filename);
 //   exposedHeaders: 'Authorization',
 //   exposedHeaders: 'user',
 // }));
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://super-plum-hippo.cyclic.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type','Authorization','user');
-  next();
-});
+  app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
+    Accept, x-client-key, x-client-token, x-client-secret, Authorization",'user');
+      next();
+    });
 
 //database
 connectDB();
