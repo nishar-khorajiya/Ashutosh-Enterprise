4ce2e75774be99697fc27d76c7fd6a83da177d97
Nishar Khorajiya <110724795+nishar-khorajiya@users.noreply.github.com>
2023-10-29 19:43:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,8 +28,7 @@ const __dirname=path.dirname(__filename);
     //Enabling CORS
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
-    Accept, x-client-key, x-client-token, x-client-secret, Authorization",'user');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, ,'user','Authorization');
       next();
     });
 
